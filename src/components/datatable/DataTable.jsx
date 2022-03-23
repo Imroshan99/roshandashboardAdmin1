524d92bd4a097c4c4e6c0aca1ea2e87769ebf3c6
Imroshan99 <roshantambe1999@gmail.com>
2022-03-23 13:37:23 +0530

    admin

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -11,7 +11,7 @@ const DataTable = () => {
       <th scope="col">Id</th>
       <th scope="col">User</th>
       <th scope="col">Email</th>
-      <th scope="col">Age</th>
+      <th scope="col">Phone</th>
       <th scope="col">Status</th>
       <th scope="col"></th>
 
@@ -20,9 +20,9 @@ const DataTable = () => {
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Roshan</td>
-      <td>roshan@gamil.com</td>
-      <td>22</td>
+      <td>Smita</td>
+      <td>smita@gamil.com</td>
+      <td>92245674</td>
       <td className="status Pending">
           Pending
       </td>
@@ -38,26 +38,63 @@ const DataTable = () => {
       <th scope="row">2</th>
       <td>jayshree</td>
       <td>jayshree@gmail</td>
-      <td>21</td>
+      <td>75045821</td>
       <td className="status Approved">
           Approved
       </td>
       <td>
+      <Link to="/users/test" style={{textDecoration:"none", color:"black"}}>
           <Button variant="outlined">View</Button>
+      </Link>
           <Button variant="outlined">Delete</Button>
       </td>
 
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td>Shyam</td>
-      <td>shyam@gmail</td>
-      <td>25</td>
+      <td>Saurabh</td>
+      <td>saurabh@gmail</td>
+      <td>99226534</td>
+      <td className="status Pending">
+             Pending
+      </td>
+      <td>
+      <Link to="/users/test" style={{textDecoration:"none", color:"black"}}>
+          <Button  variant="outlined">View</Button>
+      </Link>
+          <Button variant="outlined">Delete</Button>
+      </td>
+
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Rohan</td>
+      <td>rohan@gmail</td>
+      <td>69877458</td>
       <td className="status Rejected">
              Rejected
       </td>
       <td>
+      <Link to="/users/test" style={{textDecoration:"none", color:"black"}}>
           <Button  variant="outlined">View</Button>
+      </Link>
+          <Button variant="outlined">Delete</Button>
+      </td>
+      
+
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Kapil</td>
+      <td>kapil@gmail</td>
+      <td>9834130</td>
+      <td style={{color:"green"}} className="status Approved">
+             Approved
+      </td>
+      <td>
+      <Link to="/users/test" style={{textDecoration:"none", color:"black"}}>
+          <Button  variant="outlined">View</Button>
+      </Link>
           <Button variant="outlined">Delete</Button>
       </td>
 
